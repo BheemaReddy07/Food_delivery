447abd8373365b0f93d66e18329b2242afd3f65b
BheemaReddy07 <bheemareddy29102003@gmail.com>
2024-12-19 18:37:58 +0530

    compressed photos in public for frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 // setting an array of images with details for animation
 const headers = [
     {
-      image: "/t1.jpg",
+      image: "/t1a.jpg",
       title: "Savor Every Bite",
       description: "Experience culinary excellence with every meal. Our dishes are crafted to deliver taste and satisfaction with every bite.",
       position: { bottom: '20%', left: '5vw' },
@@ -16,7 +16,7 @@ const headers = [
       }
     },
     {
-      image: "/t2.jpg",
+      image: "/t2a.jpg",
       title: "Fresh and Local",
       description: "Enjoy farm-fresh ingredients sourced locally. Our commitment to quality ensures that every dish is made with the finest produce.",
       position: { bottom: '20%', left: '2vw' },
@@ -29,7 +29,7 @@ const headers = [
        }
     },
     {
-      image: "/t3.jpg",
+      image: "/t3a.jpg",
       title: "Deliciously Convenient",
       description: "Order your favorite meals with ease and have them delivered right to your door. Convenience meets quality in every order.",
       position: { bottom: '12%', left: '7vw' },
@@ -43,7 +43,7 @@ const headers = [
     },
      
     {
-      image: "/t5.jpg",
+      image: "/t5a.jpg",
       title: "Gourmet Delights",
       description: "Discover gourmet dishes that bring a touch of elegance to your dining experience. Perfect for special occasions or a delightful treat.",
       position: { bottom: '20%', left: '14vw' },
@@ -56,7 +56,7 @@ const headers = [
        }
     },
     {
-      image: "/t6.jpg",
+      image: "/t6a.jpg",
       title: "Healthy Choices",
       description: "Make healthier choices with our menu options that cater to a variety of dietary needs. Enjoy delicious and nutritious meals.",
       position: { bottom: '18%', left: '6vw' },
@@ -69,7 +69,7 @@ const headers = [
        }
     },
     {
-      image: "/t7.jpg",
+      image: "/t7a.jpg",
       title: "Innovative Cuisine",
       description: "Explore a menu that pushes culinary boundaries with innovative recipes and unique flavor combinations.",
       position: { bottom: '14%', left: '6vw' },
@@ -82,7 +82,7 @@ const headers = [
        }
     },
     {
-      image: "/t8.jpg",
+      image: "/t8a.jpg",
       title: "Family Favorites",
       description: "Bring the family together with meals that everyone will love. From kid-friendly dishes to hearty options, we have something for everyone.",
       position: { bottom: '20%', left: '8vw' },
@@ -95,7 +95,7 @@ const headers = [
        }
     },
     {
-      image: "/t9.jpg",
+      image: "/t9a.jpg",
       title: "Exquisite Desserts",
       description: "End your meal on a sweet note with our exquisite desserts. Each creation is a masterpiece of taste and presentation.",
       position: { bottom: '25%', right: '5vw' },
@@ -108,7 +108,7 @@ const headers = [
        }
     },
     {
-      image: "/t10.jpg",
+      image: "/t10a.jpg",
       title: "Seasonal Specials",
       description: "Enjoy dishes that celebrate the flavors of the season. Our seasonal specials are crafted to highlight the best ingredients of the time.",
       position: { bottom: '12%', left: '10vw' },
@@ -121,7 +121,7 @@ const headers = [
        }
     },
     {
-      image: "/t11.jpg",
+      image: "/t11a.jpg",
       title: "Artisan Breads",
       description: "Experience the joy of freshly baked artisan breads. Our breads are made with care and the finest ingredients for the perfect texture and flavor.",
       position: { bottom: '10%',left: '2vw' },
@@ -134,7 +134,7 @@ const headers = [
        }
     },
     {
-      image: "/t12.jpg",
+      image: "/t12a.jpg",
       title: "Signature Dishes",
       description: "Try our signature dishes that define our culinary identity. Each dish is a reflection of our passion for great food.",
       position: { bottom: '16%', left: '6vw' },
@@ -168,7 +168,7 @@ const Header = () => {
 
   return (
     <div className='header'>
-      <div className={`transition-background ${fadeIn ? 'fade-in' : 'fade-out'}`} style={{ backgroundImage: `url('/back.png')` }}></div>
+      <div className={`transition-background ${fadeIn ? 'fade-in' : 'fade-out'}`} style={{ backgroundImage: `url('/back1.jpg')` }}></div>
       <div className={`header-background ${fadeIn ? 'fade-in' : 'fade-out'}`} style={{ backgroundImage: `url(${image})` }}></div>
       <div className={`header-contents ${fadeIn ? 'fade-in' : 'fade-out'}`} style={position}>
         <h2 style={{ fontSize: font.titleFontSize, color: font.titleColor, fontWeight: font.titleFontWeight }}>{title}</h2>
